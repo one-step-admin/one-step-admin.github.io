b35faac85cbe0b6c949b70759a97c9b85d04d5f1
Hooray Hu <304327508@qq.com>
2024-03-28 09:34:43 +0800

    deploy

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,6 +1,7 @@
-import { h, nextTick, onMounted, watch } from 'vue'
+import { h, nextTick, onMounted, toRefs, watch } from 'vue'
 import Theme, { VPBadge } from 'vitepress/theme'
-import { useRoute } from 'vitepress'
+import giscusTalk from 'vitepress-plugin-comment-with-giscus'
+import { useData, useRoute } from 'vitepress'
 import './fonts/fira_code/fira_code.css'
 import './styles/var.css'
 import mediumZoom from 'medium-zoom'
@@ -28,6 +29,30 @@ export default {
       () => route.path,
       () => nextTick(() => initZoom()),
     )
+
+    // 获取前言和路由
+    const { frontmatter } = toRefs(useData())
+    // 评论组件 - https://giscus.app/
+    giscusTalk({
+      repo: 'one-step-admin/one-step-admin.github.io',
+      repoId: 'R_kgDOJpKIUg',
+      category: 'Announcements', // 默认: `General`
+      categoryId: 'DIC_kwDOJpKIUs4CeSdr',
+      mapping: 'pathname', // 默认: `pathname`
+      inputPosition: 'top', // 默认: `top`
+      lang: 'zh-CN', // 默认: `zh-CN`
+      lightTheme: 'light', // 默认: `light`
+      darkTheme: 'transparent_dark', // 默认: `transparent_dark`
+      loading: true,
+    }, {
+      frontmatter,
+      route,
+    },
+    // 是否全部页面启动评论区。
+    // 默认为 true，表示启用，此参数可忽略；
+    // 如果为 false，表示不启用。
+    // 可以在页面使用 `comment: true` 前言单独启用
+    true)
   },
   enhanceApp({ app }) {
     app.component('Badge', VPBadge)
